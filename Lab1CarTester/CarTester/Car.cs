08c7cfa86cba1bbf49aac072ee71642dd2c234df
miss-kush-kush <147203718+miss-kush-kush@users.noreply.github.com>
2023-10-11 20:32:02 +0200

    Add files via upload

---
+++ Lab1CarTester/CarTester/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CarTester
+{
+    internal abstract class Car
+    {
+        protected bool Engine = false;
+        protected bool FuelState = false;
+        protected string? CarType { get; }
+        protected string? FuelType { get; }
+
+        internal void Start()
+        {
+            if(Engine)
+            {
+                WriteLine("The car is already Started");
+            }
+            else
+            {
+                WriteLine("Starting the car");
+                Engine = true;
+            }
+        }
+
+        internal void Stop()
+        {
+            if (Engine)
+            {
+                WriteLine("Stopping the car");
+                Engine = false;
+            }
+            else
+            {
+                WriteLine("The car is already Stoped");
+            }
+        }
+
+        internal abstract void Run();
+        internal abstract void ReFuel();
+    }
+}
---
+++ Lab2Convertor/Lab2Convertor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using static System.Console;
+using Lab2Convertor;
+
+try
+{
+    string csvFile = "Table.csv";
+    string htmlFile = "Table.html";
+    Functions.GetHtmlFromCsvFile(csvFile, htmlFile);
+}
+catch (FileNotFoundException)
+{
+    WriteLine("The file was not found");
+}
+
+
+
+
