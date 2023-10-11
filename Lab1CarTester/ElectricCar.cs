fc0efa457a026e3c58e7b4e3d8a7e63231f9dca8
miss-kush-kush <147203718+miss-kush-kush@users.noreply.github.com>
2023-10-11 21:02:44 +0200

    Add files via upload

---
+++ Lab1CarTester/ElectricCar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CarTester
+{
+    internal class ElectricCar : Car
+    {
+        internal string CarType => "Eletric";
+        internal string FuelType => "Electricity";
+
+        internal override void ReFuel()
+        {
+            if (FuelState)
+            {
+
+                WriteLine($"The car is already Fueled with {FuelType}");
+
+            }
+            else
+            {
+                WriteLine($"ReFueling the car with {FuelType}");
+                FuelState = true;
+            }
+        }
+
+        internal override void Run()
+        {
+            if (Engine)
+            {
+                if (FuelState)
+                {
+                    WriteLine($"The {CarType} car is running");
+                    FuelState = false;
+                }
+                else
+                {
+                    WriteLine($"There`s no {FuelType} in {CarType} car ");
+                }
+            }
+            else
+            {
+                WriteLine("The car isn`t Started");
+            }
+        }
+    }
+}
