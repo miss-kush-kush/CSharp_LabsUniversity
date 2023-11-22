37a2de4b29f58cad6f45dd04a3412861a9a788ec
miss-kush-kush <147203718+miss-kush-kush@users.noreply.github.com>
2023-11-22 13:03:29 +0100

    Add files via upload

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label2 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(193, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 30);
+            label2.TabIndex = 1;
+            label2.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(182, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 30);
+            label1.TabIndex = 2;
+            label1.Text = "00.00.0000";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(452, 169);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+    }
+}
---
+++ Timer/Form1.cs
@@ -0,0 +1,21 @@
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("dd.MM.yyyy");
+            timer1.Start();
+        }
+    }
+}
