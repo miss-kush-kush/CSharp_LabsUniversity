45a9c3b205f65e4f157fd224f192abe42b7ae616
miss-kush-kush <147203718+miss-kush-kush@users.noreply.github.com>
2023-11-29 16:34:26 +0100

    Update Form1.cs

--- Planets/Form1.cs
+++ Planets/Form1.cs
@@ -8,11 +8,10 @@ namespace Planets
 {
     public partial class Form1 : Form
     {
-        private const int numberOfOrbits = 8;
-        private const int orbitRadiusMultiplier = 70;
-        private const int planetSize = 35;
+        private const int numberOfPlanets = 8;
+        private const int orbitRadiusMultiplier = 30;
         private const int centerX = 600;
-        private const int centerY = 600;
+        private const int centerY = 500;
         private const int sunSize = 100; 
         private RoundButton sunButton;
 
@@ -31,7 +30,7 @@ namespace Planets
         {
             sunButton = new RoundButton();
             sunButton.Width = sunButton.Height = sunSize;
-            sunButton.BackColor = Color.Yellow; // Set the color of the sun
+            sunButton.BackColor = Color.Yellow; 
             sunButton.Image = Image.FromFile("D:\\Programming\\C#\\LabsZUT\\Planets\\2k_sun.jpg");
             sunButton.Location = new Point(centerX - sunSize / 2, centerY - sunSize / 2);
             this.Controls.Add(sunButton);
@@ -39,22 +38,21 @@ namespace Planets
 
         private void InitializePlanets()
         {
-            int baseSize = 30; // Base size for Earth
-            double[] planetSizeRatios = { 0.382, 0.949, 1, 0.532, 2.209, 2.449, 1.007, 1.883 };
+            int[] planetSizes = { 20, 25, 30, 28, 50, 45, 40, 35 };
 
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < numberOfPlanets; i++)
             {
-                int planetSize = (int)(baseSize * planetSizeRatios[i]);
                 RoundButton planetButton = new RoundButton();
-                planetButton.Width = planetButton.Height = planetSize;
+                planetButton.Width = planetButton.Height = planetSizes[i];
                 planetButton.BackColor = GetPlanetColor(i);
                 planetButton.BorderColor = GetPlanetColor(i);
                 planetButton.Image = GetPlanetImage(i);
+                planetButton.Location = new Point(200 + i * 50, 200);
                 this.Controls.Add(planetButton);
                 planets[i] = planetButton;
             }
 
-            UpdatePlanetPositions(); // Set initial planet positions
+            UpdatePlanetPositions(); 
         }
 
         private Image GetPlanetImage(int index)
@@ -62,7 +60,7 @@ namespace Planets
             string[] imagePaths = 
             {
                 "D:\\Programming\\C#\\LabsZUT\\Planets\\2k_mercury.jpg",
-                "D:\\Programming\\C#\\LabsZUT\\Planets\\2k_venus.jpg",
+                "D:\\Programming\\C#\\LabsZUT\\Planets\\2k_venus_surface.jpg",
                 "D:\\Programming\\C#\\LabsZUT\\Planets\\2k_earth_daymap.jpg",
                 "D:\\Programming\\C#\\LabsZUT\\Planets\\2k_mars.jpg",
                 "D:\\Programming\\C#\\LabsZUT\\Planets\\2k_jupiter.jpg",
@@ -79,7 +77,6 @@ namespace Planets
                 }
                 catch (Exception ex)
                 {
-                    // Handle the exception if the file can't be loaded
                     Console.WriteLine($"Error loading image: {ex.Message}");
                 }
             }
@@ -89,7 +86,6 @@ namespace Planets
 
         private Color GetPlanetColor(int index)
         {
-            // Define colors for each planet
             Color[] planetColors = { Color.Gray, Color.Orange, Color.Yellow, Color.Blue, Color.Red, Color.Brown, Color.Tan, Color.Gray };
 
             if (index < planetColors.Length)
@@ -101,7 +97,7 @@ namespace Planets
         private void InitializeTimer()
         {
             timer = new Timer();
-            timer.Interval = 50; // Adjust the interval based on your preference
+            timer.Interval = 1; 
             timer.Tick += Timer_Tick;
             timer.Start();
         }
@@ -115,17 +111,17 @@ namespace Planets
         {
             double time = DateTime.Now.TimeOfDay.TotalSeconds;
 
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < numberOfPlanets; i++)
             {
-                int radius = (i + 1) * orbitRadiusMultiplier;
-                double angularVelocity = 2 * Math.PI / (60 * (i + 1)); // Adjust the divisor for different speeds
+                int radius = 80 + i * orbitRadiusMultiplier;
+                double angularVelocity = 7 * Math.PI / (60 * (i + 1)); 
 
                 double angle = angularVelocity * time;
 
                 int x = centerX + (int)(radius * Math.Cos(angle));
                 int y = centerY + (int)(radius * Math.Sin(angle));
 
-                planets[i].Location = new Point(x - planetSize / 2, y - planetSize / 2);
+                planets[i].Location = new Point(x - planets[i].Width / 2, y - planets[i].Height / 2);
             }
         }
 
@@ -134,11 +130,13 @@ namespace Planets
             Graphics g = e.Graphics;
             Pen pen = new Pen(Color.White);
 
-            // Draw orbits
-            for (int i = 1; i <= numberOfOrbits; i++)
+            for (int i = 1; i <= numberOfPlanets; i++)
             {
-                int radius = i * orbitRadiusMultiplier;
-                g.DrawEllipse(pen, centerX - radius, centerY - radius, 2 * radius, 2 * radius);
+                int radius = 50 + i * orbitRadiusMultiplier;
+                int x = centerX - radius;
+                int y = centerY - radius;
+                int diameter = 2 * radius;
+                g.DrawEllipse(pen, x, y, diameter, diameter);
             }
         }
     }
